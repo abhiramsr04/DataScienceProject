052b7fb9f1bf8f8c979d6fc65d23e7e048239dc2
Abhiram S R <abhiramsr173@gmail.com>
2025-08-11 19:23:58 +0530

    completed pipeline

--- src/datascience/components/data_ingestion.py
+++ src/datascience/components/data_ingestion.py
@@ -4,14 +4,10 @@ from src.datascience import logger
 import zipfile
 from src.datascience.entity.config_entity import (DataIngestionConfig)
 
-
-## component-Data Ingestion
-
 class DataIngestion:
     def __init__(self,config:DataIngestionConfig):
         self.config=config
     
-    # Downloading the zip file
     def download_file(self):
         if not os.path.exists(self.config.local_data_file):
             filename, headers = request.urlretrieve(
@@ -23,11 +19,6 @@ class DataIngestion:
             logger.info(f"File already exists")
 
     def extract_zip_file(self):
-        """
-        zip_file_path: str
-        Extracts the zip file into the data directory
-        Function returns None
-        """
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
